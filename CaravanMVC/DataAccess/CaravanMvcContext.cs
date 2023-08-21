f91cf93f4b182bc0d5f41fc123af23fc4de464a2
bradenasmith2 <bradenasmith@icloud.com>
2023-08-21 14:08:59 -0500

    added DbSets in Context.

--- CaravanMVC/DataAccess/CaravanMvcContext.cs
+++ CaravanMVC/DataAccess/CaravanMvcContext.cs
@@ -4,6 +4,8 @@ namespace CaravanMVC.DataAccess
 {
     public class CaravanMvcContext : DbContext
     {
+        public DbSet<Wagon> Wagons { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
         public CaravanMvcContext(DbContextOptions<CaravanMvcContext> options) : base(options)
         {
 
